bbbcc02e1256a7e3415713dfe12e733c5aee3794
Débora <debora.r.serra@gmail.com>
2024-06-19 14:06:59 -0300

    feat: add requirement 1

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,31 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * The type Pessoa.
+ */
+public abstract class Pessoa {
+
+  /**
+   * Gets nome.
+   *
+   * @return the nome
+   */
+  public String getNome() {
+    return nome;
+  }
+
+  /**
+   * Sets nome.
+   *
+   * @param nome the nome
+   */
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  /**
+   * The Nome.
+   */
+  protected String nome;
+
 }
