02a8d5f1ecc1ec663d6103087ecdabca43411184
Débora <debora.r.serra@gmail.com>
2024-06-19 16:36:13 -0300

    feat: add req 6

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,6 +1,7 @@
 package com.betrybe.sistemadevotacao;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
@@ -32,11 +33,31 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
 
   @Override
   public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
-
+    for (String cpf : cpfsComputados) {
+      if (Objects.equals(cpf, cpfPessoaEleitora)) {
+        System.out.println("Pessoa eleitora já votou!");
+        return;
+      }
+    }
+    for (PessoaCandidata candidate : pessoasCandidatas) {
+      if (candidate.getNumero() == numeroPessoaCandidata) {
+        candidate.receberVoto();
+      }
+    }
+    cpfsComputados.add(cpfPessoaEleitora);
   }
 
   @Override
   public void mostrarResultado() {
-
+    if (cpfsComputados.isEmpty()) {
+      System.out.println("É preciso ter pelo menos um voto para mostrar o resultado.");
+    }
+    int total = cpfsComputados.size();
+    for (PessoaCandidata candidate : pessoasCandidatas) {
+      double percent = (candidate.getVotos() / total) * 100;
+      System.out.printf("Nome: %s - %i votos (%.2d%)%n", candidate.getNome(), candidate.getVotos(),
+          percent);
+    }
+    System.out.println("Total de votos: " + total);
   }
 }
